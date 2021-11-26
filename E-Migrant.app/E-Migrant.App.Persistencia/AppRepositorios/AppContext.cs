8c59afdc87ca7a75149019b929eb4ba7f4e96b19
YeinsM <wildermancera1@gmail.com>
2021-11-26 15:04:10 -0400

    Completo AppContext

--- E-Migrant.app/E-Migrant.App.Persistencia/AppRepositorios/AppContext.cs
+++ E-Migrant.app/E-Migrant.App.Persistencia/AppRepositorios/AppContext.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using E_Migrant.App.Dominio.Entidades;
 
 namespace E_Migrant.App.Persistencia.AppRepositorios
 {
-    public class AppContext
+    public class AppContext : DbContext
     {
-        
+        public DbSet<Persona> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if(!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = E-Migrant");
+            }
+        }
+
+        // Manejo de Tablas Intermedias
+        //  protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<TablaIntermedia>().HasKey(x=> new{x.Tabla1,x.Tabla2});
+        // }        
     }
 }
